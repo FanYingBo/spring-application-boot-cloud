72fc18e844523b431453d85964284e01bfe56457
FanYingBo <15216814563@163.com>
2020-04-26 16:24:16 +0800

    更改一些错误的依赖

--- spring-application/src/main/java/com/study/selfs/gupao/springboot/CustomConfigApplication.java
+++ spring-application/src/main/java/com/study/selfs/gupao/springboot/CustomConfigApplication.java
@@ -1,12 +1,12 @@
 package com.study.selfs.gupao.springboot;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CustomConfigApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CustomConfigApplication.class,args);
+        new SpringApplicationBuilder(CustomConfigApplication.class).web(WebApplicationType.SERVLET).build(args).run();
     }
 }
--- spring-application/src/main/java/com/study/selfs/gupao/springboot/CustomController.java
+++ spring-application/src/main/java/com/study/selfs/gupao/springboot/CustomController.java
@@ -1,18 +1,13 @@
 package com.study.selfs.gupao.springboot;
 
-import com.study.selfs.gupao.custom.springboot.starter.CustomConfig;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomController {
 
-    @Autowired
-    private CustomConfig customConfig;
-
     @GetMapping("/custom")
-    public String  getCustomConfig(){
-        return "custom config value = " + customConfig.getConfig();
+    public String  getValue(){
+        return "custom config value = ";
     }
 }
